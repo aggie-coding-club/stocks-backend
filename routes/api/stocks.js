ff3452a4983c279141e76447847c1527924bb25f
YonathanZetune <yonathan@zetune.com>
2021-01-21 16:31:14 -0600

    set ticker data for stock

--- routes/api/stocks.js
+++ routes/api/stocks.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Ticker = require("../../models/ticker");
+const TickerData = require("../../models/tickerData");
 
 //@desc     Add stock ticker to db, update if not in it
-//@route    GET /api/stocks
+//@route    POST /api/stocks/addStockDB
 router.post("/addStockDB", async (req, res) => {
   const newTicker = {
     symbol: req.body.symbol,
@@ -17,11 +18,66 @@ router.post("/addStockDB", async (req, res) => {
   try {
     let ticker = await Ticker.findOne({ symbol: req.body.symbol });
     if (ticker) {
-      res.json({ msg: "Ticker is already in the DB!" });
+      res.json({
+        msg: `Ticker with symbol ${req.body.symbol} is already in the DB!`,
+      });
     } else {
       let ticker = await Ticker.create(newTicker);
       ticker.save();
       res.json({ msg: "Ticker created successfully" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.json({ error: err });
+  }
+});
+
+//@desc     Set the tickerData object in the DB fo rthe correlation ticker
+//@route    POST /api/stocks/setStockData
+router.post("/setStockData", async (req, res) => {
+  let symbol = req.body.symbol;
+
+  if (!symbol) {
+    return res
+      .status(400)
+      .json({ msg: "Missing symbol parameter in request!" });
+  }
+  try {
+    let ticker = await Ticker.findOne({ symbol: req.body.symbol });
+    if (ticker) {
+      const tickerData = {
+        ticker: ticker,
+        data: req.body.data,
+      };
+      var currData = await TickerData.findOne({ ticker: ticker });
+      if (currData) {
+        //ticker data already exists, we should just update the data at this point
+        TickerData.updateOne(
+          { ticker: ticker },
+          { data: req.body.data },
+
+          // {
+          //   $push: { tvs: res },
+          // },
+          { multi: true },
+          function (err, docs) {
+            if (err) {
+              console.log(err);
+            } else {
+              //   console.log("Original Doc : ", docs);
+            }
+          }
+        );
+        res.json({
+          msg: `Data with symbol ${req.body.symbol} exists, data updated!`,
+        });
+      } else {
+        let data = await TickerData.create(tickerData);
+        data.save();
+        res.json({
+          msg: `Data with symbol ${req.body.symbol} created!`,
+        });
+      }
     }
   } catch (err) {
     console.error(err);
